0dab6d610ddda539115ed243ee9f391f103b9816
Ifeanyi-Owah <ifeanyi.owah.io@gmail.com>
2020-07-04 20:11:21 -0400

    Chore: sort leaderboard

--- src/components/Leader.js
+++ src/components/Leader.js
@@ -3,24 +3,65 @@ import { connect } from "react-redux";
 
 class Leader extends Component {
   render() {
-    console.log(this.props);
-    const { user } = this.props;
+    const {
+      name,
+      answeredQuestions,
+      createdQuestions,
+      avatarUrl,
+      score,
+    } = this.props;
+    console.log(answeredQuestions, createdQuestions);
+
     return (
       <li className="list-group-item shadow pt-3 pb-3 mb-5 bg-white rounded card">
         <div className="d-flex">
           <img
             className="rounded-circle w-25 ml-2 mr-2"
-            src={user.avatarURL}
-            alt={`Avatar of ${user.name}`}
+            src={avatarUrl}
+            alt={`Avatar of ${name}`}
           />
           <div className="mr-5">
-            <h2>{user.name}</h2>
-            <p>Anwered questions</p>
-            <p>Unanswered question</p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">{name}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Anwered questions</td>
+                  {/* <td>Otto</td> */}
+                  <td>{answeredQuestions}</td>
+                </tr>
+                <tr>
+                  <td>Created questions</td>
+                  {/* <td>Thornton</td> */}
+                  <td>{createdQuestions}</td>
+                </tr>
+              </tbody>
+            </table>
+            {/* <h2>{user.name}</h2>
+            <p>{`Anwered questions${answeredQuestions}`}</p>
+            <p>{`Created questions${createdQuestions}`}</p> */}
           </div>
           <div className="card ml-5">
-            <h3 className="card-header">Score</h3>
-            <div className="btn rounded-circle bg-success">10</div>
+            {/* <h3 className="card-header">Score</h3>
+            <div className="btn rounded-circle bg-success">
+              {answeredQuestions + createdQuestions}
+            </div> */}
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {/* <td>Otto</td> */}
+                  <td>{score}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </li>
@@ -28,19 +69,9 @@ class Leader extends Component {
   }
 }
 
-function mapStateToProps({ authedUser, users, questions }, { id }) {
-  const user = users[id];
-
+function mapStateToProps({ authedUser }) {
   return {
     authedUser,
-    user,
-    // question: question
-    //   ? formatQuestion({
-    //       optionOneText: question.optionOne.text,
-    //       optionTwoText: question.optionTwo.text,
-    //       author: question.author,
-    //     })
-    //   : null,
   };
 }
 
--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -7,21 +7,42 @@ class LeaderBoard extends Component {
     return (
       <div style={{ width: "700px", margin: "0 auto" }}>
         <ul className="list-group mt-2 p-3">
-          {this.props.userId.map((id) => (
-            <Leader id={id} key={id} />
-          ))}
-          {/* <Leader />
-        <Leader />
-        <Leader /> */}
+          {this.props.userLeader
+            .map((user) => (
+              <Leader
+                id={user.id}
+                key={user.id}
+                name={user.name}
+                avatarUrl={user.avatarURL}
+                answeredQuestions={user.answeredQuestions}
+                createdQuestions={user.createdQuestions}
+                score={user.score}
+              />
+            ))
+            .sort((a, b) => b.store - a.store)}
         </ul>
       </div>
     );
   }
 }
 
-function mapStateToProps({ users }) {
+function mapStateToProps({ users, authedUser }) {
+  const userId = Object.keys(users);
+  const userLeader = userId
+    .map((id) => {
+      return {
+        ...users[id],
+        answeredQuestions: Object.keys(users[id].answers).length,
+        createdQuestions: users[id].questions.length,
+        score:
+          Object.keys(users[id].answers).length + users[id].questions.length,
+      };
+    })
+    .sort((a, b) => b.score - a.score);
+  console.log(userLeader);
   return {
-    userId: Object.keys(users),
+    userLeader,
+    authedUser,
   };
 }
 
