b31ef9a6a76121a522111a327b81de8d042aaad9
Ifeanyi-Owah <ifeanyi.owah.io@gmail.com>
2020-08-03 13:25:18 -0400

    Refactor: modify login feat

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,23 +2,20 @@ import React, { Component } from "react";
 import { Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
-class ProtectedRoute extends Component {
-  render() {
-    const { children, authedUser, ...rest } = this.props;
-    console.log(authedUser);
-    return (
-      <Route
-        {...rest}
-        render={({ location }) => {
-          return rest.authedUser ? (
-            children
-          ) : (
-            <Redirect to={{ pathname: "/login", state: { from: location } }} />
-          );
-        }}
-      />
-    );
-  }
+function ProtectedRoute({ children, authedUser, ...rest }) {
+  console.log(authedUser);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return authedUser ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: "/login", state: { from: location } }} />
+        );
+      }}
+    />
+  );
 }
 
 function mapStateToProps({ authedUser }) {
